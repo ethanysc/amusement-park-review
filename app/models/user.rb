cbe02fd761a44a2b793d03aa7877b9a10b442328
Ethan Yu-Sheng Chang <ethanysc@github.com>
2018-07-23 16:34:38 -0400

    user can sign in with username and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,15 @@
 class User < ApplicationRecord
-  attr_writer :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  def login
-    @login || self.username || self.email
-  end
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
-  end
-
-  validate :validate_username
-
-  def validate_username
-    if User.where(email: username).exists?
-      errors.add(:username, :invalid)
-    end
-  end
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:username]
+
+  def will_save_change_to_email?
+    false
+  end
 end
