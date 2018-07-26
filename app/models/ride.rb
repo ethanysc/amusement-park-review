42a37a5708c94ececd92591ffd1b0641b1610523
Alex Smith <queso@Alexs-MacBook-Air.local>
2018-07-26 14:09:24 -0400

    fixed typo

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,5 +5,5 @@ class Ride < ApplicationRecord
   has_many :ride_catalogues
   has_many :ride_features, through: :ride_catalogues
   has_many :ride_reviews
-  has_many :user, through: :ride_reviews 
+  has_many :users, through: :ride_reviews
 end
