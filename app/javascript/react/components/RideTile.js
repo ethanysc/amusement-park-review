aa70d4fd8b2d449cac7476f95fe7c105a5d73e86
Ethan Yu-Sheng Chang <ethanysc@github.com>
2018-07-27 16:41:47 -0400

    spec

--- app/javascript/react/components/RideTile.js
+++ app/javascript/react/components/RideTile.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const RideTile = props => {
 
   return(
     <div>
-      <a href={`/amusement_parks/${props.parkId}/rides/${props.id}`}>{props.name}</a>
+      <Link to={`/amusement_parks/${props.parkId}/rides/${props.id}`}>{props.name}</Link>
     </div>
   )
 }
