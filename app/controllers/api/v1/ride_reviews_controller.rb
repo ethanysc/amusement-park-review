52a7ab8d42d93d5d84bb127edb536f491809db76
Brianna Kincart <briannakincart@gmail.com>
2018-08-01 14:58:58 -0400

    Revert "Create upvoting"

--- app/controllers/api/v1/ride_reviews_controller.rb
+++ app/controllers/api/v1/ride_reviews_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::RideReviewsController < ApiController
 
   end
 
+
   def new
     new_ride_review = RideReview.new
   end
@@ -11,7 +12,7 @@ class Api::V1::RideReviewsController < ApiController
     new_ride_review = RideReview.new(ride_review_params)
     new_ride_review.user = current_user
     new_ride_review.ride_id = params[:rideId]
-    
+
     if new_ride_review.save
       render json: { ride_review: RideReviewSerializer.new(new_ride_review) }
     else
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,25 +1,6 @@
 class Review < ApplicationRecord
-  validates :overall_rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-
-  has_many :user_votes
-
+  validates :overall_rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  
   belongs_to :user
   belongs_to :amusement_park
-
-  def tally_likes
-    likes = 0
-    self.user_votes.each do |vote|
-      likes += 1 if vote.vote > 0
-    end
-    likes
-  end
-
-  def tally_dislikes
-    dislikes = 0
-    self.user_votes.each do |vote|
-      dislikes += 1 if vote.vote > 0
-    end
-    dislikes
-  end
-
 end
