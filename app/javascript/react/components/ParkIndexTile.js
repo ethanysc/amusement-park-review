d05dd4165383dbe88b9a6e938d05e54cb524ed38
Alex Smith <queso@Alexs-MacBook-Air.local>
2018-08-02 18:20:55 -0400

    minor merge conflicts fixed

--- app/javascript/react/components/ParkIndexTile.js
+++ app/javascript/react/components/ParkIndexTile.js
@@ -6,13 +6,15 @@ const ParkIndexTile = props => {
   if(props.image.url){
     image = <img src={props.image.url} alt="profile-sample2"/>
   }
+
   return(
-    <figure className="snip1581">{image}
+    <figure className="snip1581"><img src="https://lunaparknyc.com/app/uploads/2018/03/LP_website-header-ConeyIslandCyclone.jpg" alt="profile-sample2"/>
       <figcaption>
         <h3 className="title3">{props.name}</h3>
       </figcaption>
       <Link to={`/amusement_parks/${props.id}`} />
     </figure>
+
   )
 }
 
